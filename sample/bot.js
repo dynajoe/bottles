39954a5a82872222b06b5130429b64ba6a3aa3da
Joe Andaverde <joe@andaverde.net>
2013-07-14 12:39:42 -0500

    ticking with a new object

--- sample/bot.js
+++ sample/bot.js
@@ -9,12 +9,11 @@ Bot.prototype.tick = function (sensors, cb) {
    };
 
    if (sensors.radar.length > 0) {
-
       commands.radar_heading = sensors.radar[0].heading;
       commands.heading = commands.radar_heading;
       commands.turret_heading = commands.radar_heading;
    } else {
-      commands.radar_heading = Math.PI / 4
+      commands.radar_heading = sensors.radar_heading + Math.PI / 4
       commands.heading = commands.radar_heading;
       commands.turret_heading = commands.radar_heading;
    }
