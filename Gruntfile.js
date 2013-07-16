2ed036d632fe30a34e4fb1929701ba70deab0e1a
Joe Andaverde <joe@andaverde.net>
2013-07-16 12:27:27 -0500

    Added jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,35 @@
 module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-simple-mocha');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
   grunt.initConfig({
-    simplemocha:{
+    simplemocha: {
       options: {
         reporter: 'spec',
         slow: 200,
         timeout: 1000,
       },
       all: {
-        src: ['test/util.js']
+        src: ['test/**/*.js']
       }
     },
-    watch:{
-      all:{
-        files:['!node_modules', '**/*.js'],
-        tasks:['test']
+    watch: {
+      tests: {
+        files: ['!node_modules', '**/*.js'],
+        tasks: ['simplemocha']
+      }
+    },
+    jshint: {
+      src: {
+        options: {
+          jshintrc: '.jshintrc'
+        },
+        src: ['lib/**/*.js']
       }
     }
   });
-
+  grunt.registerTask('default', ['jshint', 'test']);
   grunt.registerTask('test', 'simplemocha');
-  grunt.registerTask('watch', 'watch');
+  grunt.registerTask('watch', 'watch,tests');
 };
