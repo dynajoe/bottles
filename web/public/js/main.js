3346a612a6fc57e0e97d13b8f12107f4b27880d9
Joe Andaverde <joe@andaverde.net>
2013-08-03 13:04:13 -0500

    Added the ability to update brain

--- web/public/js/main.js
+++ web/public/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-   var code = localStorage.getItem('code');  
-   
+   var code = localStorage.getItem('code');
+
    if (code) $('#code').val(code);
 
    var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
@@ -10,15 +10,15 @@ $(document).ready(function () {
       continueComments: "Enter",
       extraKeys: {"Ctrl-Q": "toggleComment"}
    });
- 
+
    var socket = io.connect('/');
    var viewer = new GameViewer();
 
    $('#viewer').append(viewer.view);
-   
-   var is_started = false;   
+
+   var is_started = false;
    var brain = new KeyboardBrain();
-   
+
    socket.on('start', function (data) {
       if (!is_started) {
          viewer.set_data(data);
@@ -32,7 +32,7 @@ $(document).ready(function () {
 
    socket.on('brain_tick', function (sensors) {
       brain.tick(sensors, function (cmds) {
-         socket.emit('brain_tick', cmds);   
+         socket.emit('brain_tick', cmds);
       });
    });
 
@@ -42,40 +42,49 @@ $(document).ready(function () {
 
    var is_joined = false;
 
+   var update_brain = function () {
+      var code = editor.getDoc().getValue();
+      brain = wrap_brain(code);
+
+   };
+
    $('#brain-editor .save').click(function () {
       var code = editor.getDoc().getValue();
-      localStorage.setItem('code', code);  
+      localStorage.setItem('code', code);
    });
-   
+
    $('#brain-editor .restart').click(function () {
       socket.emit('restart');
    });
 
+   $('#brain-editor .update').click(function () {
+     update_brain();
+   });
+
    $('#waiting a.add-comp').click(function () {
-      socket.emit('add_comp');   
+      socket.emit('add_comp');
    });
 
    $('#waiting a.start').click(function () {
-      socket.emit('start');   
+      socket.emit('start');
    });
 
    $('#join-game form').submit(function () {
       var $this = $(this);
-      
-      var code = editor.getDoc().getValue();
-      brain = wrap_brain(code);
-      
+
+      update_brain();
+
       $('#join-game').addClass('hide');
 
       socket.emit('join', $('input', $this).val());
 
       return false;
-   });   
+   });
 
    var wrap_brain = function (code) {
       var module = { exports: {} };
       var exports = module.exports;
-      
+
       (function (module, exports) {
          eval(code);
       })(module, exports);
